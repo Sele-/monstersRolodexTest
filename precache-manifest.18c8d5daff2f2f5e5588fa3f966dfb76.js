4ba0fc8c558457170b0ff06bc8e06ce2dda1ecf9
andrea.g.moreno <andrea.g.moreno@accenture.com>
2020-01-06 09:48:12 -0300

    Updates

---
+++ precache-manifest.18c8d5daff2f2f5e5588fa3f966dfb76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab898a4836ff447fe06652a47b8b7d03",
+    "url": "/monstersRolodexTest/index.html"
+  },
+  {
+    "revision": "21ee4f0c17101834b38b",
+    "url": "/monstersRolodexTest/static/css/main.75b8cda7.chunk.css"
+  },
+  {
+    "revision": "25f4a5800515bf04f7ae",
+    "url": "/monstersRolodexTest/static/js/2.6e883175.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monstersRolodexTest/static/js/2.6e883175.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21ee4f0c17101834b38b",
+    "url": "/monstersRolodexTest/static/js/main.4fdc5327.chunk.js"
+  },
+  {
+    "revision": "d5798352d6ef04dbd19d",
+    "url": "/monstersRolodexTest/static/js/runtime-main.94278b4e.js"
+  }
+]);
